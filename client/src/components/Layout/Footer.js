5f8c0623c5146cab2bc676fea30fd5fee846c117
Bruch-Wayne <powerdggy@gmail.com>
2023-05-04 18:07:18 +0600

    error fixed footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <h1 className="text-center">
-        All Right Reserved &copy; {currentYear} ELECTRO<span>BAY</span>.{" "}
+        All Right Reserved &copy; {currentYear} ELECTRO<span>BAY</span>.
       </h1>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
