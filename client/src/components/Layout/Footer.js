296e198caf68308cad3f2030adb9b55404571085
Bruch-Wayne <powerdggy@gmail.com>
2023-05-04 17:54:15 +0600

    error fixed footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; {currentYear} </h1>
+      <h1 className="text-center">
+        All Right Reserved &copy; {currentYear} ELECTRO<span>BAY</span>.{" "}
+      </h1>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
